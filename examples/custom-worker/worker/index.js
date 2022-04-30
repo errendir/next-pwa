ad379af77e3be404958b881a64ca584d72640667
Albert Sławiński <errendir@gmail.com>
2022-04-30 19:29:59 +0200

    Example of the custom worker compilation bug

--- examples/custom-worker/worker/index.js
+++ examples/custom-worker/worker/index.js
@@ -1,13 +1,10 @@
-'use strict'
-
 // To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
 // https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
 //
 // self.__WB_DISABLE_DEV_LOGS = true
 
-const util = require('./util')
-
-util()
+import * as hub from "./hub"
+console.log(hub)
 
 // listen to message event from window
 self.addEventListener('message', event => {
